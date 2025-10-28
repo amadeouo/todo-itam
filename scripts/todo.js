80329afceebe3bda715c0ad75ec7ebd89e63a303
Mikhail Volodko <clavor.32@gmail.com>
2025-10-28 22:20:02 +0300

    final result

--- scripts/todo.js
+++ scripts/todo.js
@@ -15,10 +15,15 @@ class Todo {
     addTodoCloseButton: "[data-js-todo-modal-close-button]",
     todoInput: "[data-js-todo-input-checkbox]",
     todoForm: "[data-js-todo-form]",
+    todoLabel: "[data-js-todo-label]",
     todosList: "[data-js-todo-todos-wrapper]",
     emptyMessage: "[data-js-empty-message]",
     searchForm: "[data-js-todo-search-form]",
     searchInput: "[data-js-todo-search-form-input]",
+    editTodosButton: "[data-js-todo-edit-todos-button]",
+    deleteTodosButton: "[data-js-todo-delete-todos-button]",
+    notificationDelete: "[data-js-todo-notification-delete]",
+    counterNumber: "[data-js-todo-number-counter]",
   }
 
   stateClasses = {
@@ -27,6 +32,7 @@ class Todo {
     darkTheme: "dark", // theme-switcher
     isOpen: "is-open", // modal -> addTodoModalElement
     isVisible: "is-visible", // empty message
+    isVisibleNotification: "is-visible-notification" // notification delete
   }
 
   localStorageKeys = {
@@ -63,12 +69,19 @@ class Todo {
     this.searchFormElement = this.rootElement.querySelector(this.selectors.searchForm)
     this.searchInputElement = this.searchFormElement.querySelector(this.selectors.searchInput)
 
+    // notification-delete
+    this.notificationDeleteElement = this.rootElement.querySelector(this.selectors.notificationDelete)
+    this.counterNumberElement = this.notificationDeleteElement.querySelector(this.selectors.counterNumber)
+
     this.state = {
       items: this.getTodoFromLocalStorage(),
       filter: {
         textFilter: null,
         selectorFilter: null,
       },
+      deletedTodos: [],
+      deleteTimeout: null,
+      counterInterval: null,
     }
 
     this.render()
@@ -161,12 +174,9 @@ class Todo {
   // add Todos
   createTodo({title, id, checked}) {
     try {
-      if (!title) {
-        throw new Error()
-      }
-
       const todo = document.createElement("form")
       todo.classList.add("form")
+      todo.setAttribute("data-todo-id", id)
       if (checked) {
         todo.classList.add("form--checked")
       }
@@ -182,6 +192,7 @@ class Todo {
         <label
           class="form-input__title"
           for="${id}"
+          data-js-todo-label
         >
           ${title}
         </label>
@@ -190,6 +201,7 @@ class Todo {
         <button
           class="form-buttons-edit"
           type="button"
+          data-js-todo-edit-todos-button
         >
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736" stroke="#CDCDCD" stroke-linecap="round" stroke-linejoin="round"/>
@@ -198,6 +210,7 @@ class Todo {
         <button
           class="form-buttons-delete"
           type="button"
+          data-js-todo-delete-todos-button
         >
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3.87414 7.61505C3.80712 6.74386 4.49595 6 5.36971 6H12.63C13.5039 6 14.1927 6.74385 14.1257 7.61505L13.6064 14.365C13.5463 15.1465 12.8946 15.75 12.1108 15.75H5.88894C5.10514 15.75 4.45348 15.1465 4.39336 14.365L3.87414 7.61505Z" stroke="#CDCDCD"/>
@@ -215,6 +228,154 @@ class Todo {
     }
 
   }
+
+
+  // Todos functionals button
+  deleteTodo(id) {
+    const indexTodo = this.state.items.findIndex(item => item.id === id)
+    if (indexTodo === -1) return
+
+    const deletedTodo = this.state.items[indexTodo]
+    this.state.deletedTodos.push({
+      todo: deletedTodo,
+      index: indexTodo,
+    })
+
+    this.state.items.splice(indexTodo, 1)
+
+    this.saveTodoToLocalStorage()
+    this.render()
+  }
+
+  // Deleting
+  processDeleting(id) {
+    this.deleteTodo(id)
+    this.showUndoNotification()
+  }
+
+  showUndoNotification() {
+    if (this.state.deleteTimeout) {
+      clearTimeout(this.state.deleteTimeout)
+    }
+    if (this.state.counterInterval) {
+      clearInterval(this.state.counterInterval)
+    }
+    this.notificationDeleteElement.classList.add(this.stateClasses.isVisibleNotification)
+    this.startNumberCounter()
+
+    this.state.deleteTimeout = setTimeout(() => {
+      this.confirmDeletion()
+      this.hideUndoNotification()
+    }, 5000)
+  }
+
+  confirmDeletion() {
+    this.state.deletedTodos = []
+
+    if (this.state.deleteTimeout) {
+      clearTimeout(this.state.deleteTimeout)
+      this.state.deleteTimeout = null
+    }
+  }
+
+  hideUndoNotification() {
+    this.notificationDeleteElement?.classList.remove(this.stateClasses.isVisibleNotification)
+
+    if (this.state.counterInterval) {
+      clearInterval(this.state.counterInterval)
+      this.state.counterInterval = null
+    }
+  }
+
+  startNumberCounter() {
+    let numberValue  = 5
+
+    this.counterNumberElement.innerHTML = numberValue
+
+    this.state.counterInterval = setInterval(() => {
+      numberValue--
+
+      if (this.counterNumberElement) {
+        this.counterNumberElement.innerHTML = numberValue
+      }
+
+      if (numberValue === 0) {
+        clearInterval(this.state.counterInterval)
+        this.state.counterInterval = null
+      }
+    }, 1000)
+  }
+
+  undoDelete() {
+    if (this.state.deletedTodos.length === 0) return
+
+    this.state.deletedTodos.reverse().forEach(({ todo, index }) => {
+      if (index <= this.state.items.length) {
+        this.state.items.splice(index, 0, todo)
+      } else {
+        this.state.items.push(todo)
+      }
+    })
+
+    this.state.deletedTodos = []
+
+    if (this.state.deleteTimeout) {
+      clearTimeout(this.state.deleteTimeout)
+      this.state.deleteTimeout = null
+    }
+
+    this.saveTodoToLocalStorage()
+    this.hideUndoNotification()
+    this.render()
+  }
+
+  editTodo(id) {
+    const todo = this.state.items.find(item => item.id === id)
+    if (!todo) return
+
+    const todoForm = this.todosListElement.querySelector(`[data-todo-id="${id}"]`)
+    if (!todoForm) return
+
+    const label = todoForm.querySelector(this.selectors.todoLabel)
+    if (!label) return
+
+    const currentTitle = todo.title
+
+    const editInput = document.createElement('input')
+    editInput.type = 'text'
+    editInput.value = currentTitle
+    editInput.className = 'form-input__edit'
+
+    label.replaceWith(editInput)
+    editInput.focus()
+    editInput.select()
+
+    const saveEdit = () => {
+      const newTitle = editInput.value.trim()
+
+      if (newTitle !== currentTitle) {
+        todo.title = newTitle
+        this.saveTodoToLocalStorage()
+        this.render()
+      } else if (!newTitle) {
+        this.render()
+      } else {
+        this.render()
+      }
+    }
+
+    editInput.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault()
+        saveEdit()
+      } else if (e.key === 'Escape') {
+        this.render()
+      }
+    })
+
+    editInput.addEventListener('blur', saveEdit)
+  }
+
 
   // arrow-functions
 
@@ -296,7 +457,10 @@ class Todo {
       todo.checked = isChecked
       this.saveTodoToLocalStorage()
     }
-    this.render()
+    const timeToDelete = setInterval(() => {
+      this.render()
+      clearInterval(timeToDelete)
+    }, 400)
   }
 
   onSearchFormSubmit = (e) => {
@@ -305,7 +469,6 @@ class Todo {
 
   onSearchInput = ({ target }) => {
     const value = target.value.trim()
-    console.log(value)
     if (value.length > 0) {
       this.state.filter.textFilter = value
       this.render()
@@ -313,6 +476,29 @@ class Todo {
       this.state.filter.textFilter = ""
       this.render()
     }
+  }
+
+  onClickDeleteTodoButton = (e) => {
+    const todoElement = e.target.closest('.form')
+    if (!todoElement)  return
+    const idTodo = todoElement.querySelector(this.selectors.todoInput)
+    if (idTodo) {
+      this.processDeleting(idTodo.id)
+    }
+  }
+
+  onClickEditTodoButton = (e) => {
+    const todoElement = e.target.closest(".form")
+    if (!todoElement) return
+
+    const idTodo = todoElement.getAttribute("data-todo-id")
+    if (idTodo) {
+      this.editTodo(idTodo)
+    }
+  }
+
+  onClickUndoButton = () => {
+    this.undoDelete()
   }
 
   init() {
@@ -329,6 +515,15 @@ class Todo {
     this.todosListElement.addEventListener("change", this.onCheckedTodo)
     this.searchFormElement.addEventListener("submit", this.onSearchFormSubmit)
     this.searchInputElement.addEventListener("input", this.onSearchInput)
+    this.todosListElement.addEventListener("click", (e) => {
+      if (e.target.closest(this.selectors.deleteTodosButton)) {
+        this.onClickDeleteTodoButton(e)
+      }
+      if (e.target.closest(this.selectors.editTodosButton)) {
+        this.onClickEditTodoButton(e)
+      }
+    })
+    this.notificationDeleteElement?.addEventListener("click", this.onClickUndoButton)
   }
 }
 
